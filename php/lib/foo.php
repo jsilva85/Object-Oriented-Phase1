54e64e623b627ef8138bc5c99ad98dfb40fe4544
Jesse Silva <thebestjesse76@gmail.com>
2019-04-25 13:50:20 -0600

    object oriented phase1

--- php/lib/foo.php
+++ php/lib/foo.php
@@ -2,6 +2,6 @@
 <?php
 require_once(dirname(__DIR__) . "/vendor/autoload.php");
 require_once(dirname(__DIR__) . "/classes/autoload.php");
-use dginsburg\ObjectOriented;
+use jsilva85\ObjectOrientedphase1;
 $authorId = new Author("06083f41-e334-4caf-ad7e-9b2600c41793", 'books.com', 'a125bc814cff43d68bfcbc6570ad31ac', 'arnold@cnm.edu', '$argon2i$v=19$m=1024,t=384,p=2$T1B6Ymdqa3FJdmZqaDdqYg$hhyC1jf2WjbgfD8Jp6GZE9Tg3IpsYpXKm2VWYOJq8LA', 'username;');
 var_dump($authorId);
